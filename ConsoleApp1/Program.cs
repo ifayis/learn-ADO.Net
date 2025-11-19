85ccffbf1a378c4d7004920c1c2bc19eb33982c0
Fayis KV <mohammedfayis135@gmail.com>
2025-11-19 17:32:28 +0530

    ADO

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string connstr = @"Server=localhost\SQLEXPRESS;Database=StudentsDB;Trusted_Connection=True;";
+            using (SqlConnection con = new SqlConnection(connstr))
+            {
+                con.Open();
+
+                string query = "select id,name,detailsid from Students";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Console.WriteLine($"{dr["id"]} - {dr["name"]} - {dr["detailsid"]}");
+                }
+                dr.Close();
+            }
         }
     }
 }
