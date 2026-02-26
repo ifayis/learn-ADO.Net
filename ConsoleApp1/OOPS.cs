2a540deaffefed4f5a97abc1cba19aded4dc5cfa
Fayis KV <mohammedfayis135@gmail.com>
2026-02-26 15:37:42 +0530

    Inheritace

---
+++ ConsoleApp1/OOPS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class OOPS
+    {
+        // INHERITANCE
+        public class Animal
+        {
+            public void Jumb()
+            {
+                Console.WriteLine("The Animal Jumbed");
+            }
+        }
+
+        public class Dog : Animal
+        {
+            public void Bark()
+            {
+                Console.WriteLine("Its Barking Very Well");
+            }
+        }
+
+        static void Main()
+        {
+            Dog A = new Dog();
+            A.Bark();
+            A.Jumb();
+        }
+    }
+}
