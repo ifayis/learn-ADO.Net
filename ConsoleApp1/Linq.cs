9c33b721a003417b84313de2f5d06c5089b03521
Fayis KV <mohammedfayis135@gmail.com>
2026-02-26 15:38:30 +0530

    Linq

--- ConsoleApp1/Linq.cs
+++ ConsoleApp1/Linq.cs
@@ -13,26 +13,26 @@ namespace ConsoleApp1
         public string Department { get; set; }
     }
 
-    class Linq
-    {
-        static void Main()
-        {
-            List<Student> students = new List<Student>()
-            {
-            new Student {Name = "mhd", Age = 20, Department = "hr"},
-            new Student {Name = "fys", Age = 22, Department = "it"},
-            new Student {Name = "abc", Age = 24, Department = "marketing"}
-            };
+    //class Linq
+    //{
+    //    static void Main()
+    //    {
+    //        List<Student> students = new List<Student>()
+    //        {
+    //        new Student {Name = "mhd", Age = 20, Department = "hr"},
+    //        new Student {Name = "fys", Age = 22, Department = "it"},
+    //        new Student {Name = "abc", Age = 24, Department = "marketing"}
+    //        };
 
-            //var result = students.GroupBy(a => a.Age).OrderByDescending(s => s.Key).Skip(1).FirstOrDefault().ToList();
+    //        //var result = students.GroupBy(a => a.Age).OrderByDescending(s => s.Key).Skip(1).FirstOrDefault().ToList();
 
-            //foreach (var stu in result)
-            //{
-            //    Console.WriteLine($"{stu.Name} - {stu.Age} - {stu.Department}");
-            //}
+    //        //foreach (var stu in result)
+    //        //{
+    //        //    Console.WriteLine($"{stu.Name} - {stu.Age} - {stu.Department}");
+    //        //}
 
-            int result = students.Max(s => s.Age);
-            Console.WriteLine(result);
-        }
-    }
+    //        int result = students.Max(s => s.Age);
+    //        Console.WriteLine(result);
+    //    }
+    //}
 }
